93858fbaa281ff163c39ead55edf7d6c60d8b751
vegter <amsjurien@gmail.com>
2020-02-11 13:44:49 +0100

    Show db summary info

--- src/services/gob_api.js
+++ src/services/gob_api.js
@@ -1,9 +1,11 @@
 import { get_gob_api } from "./api";
 
 export async function get_db_locks() {
-  return fetch(get_gob_api() + "gob/info/locks").json();
+  const locks = await fetch(get_gob_api() + "gob/info/locks");
+  return locks.json();
 }
 
 export async function get_db_queries() {
-  return fetch(get_gob_api() + "gob/info/activity").json();
+  const activities = await fetch(get_gob_api() + "gob/info/activity");
+  return activities.json();
 }
