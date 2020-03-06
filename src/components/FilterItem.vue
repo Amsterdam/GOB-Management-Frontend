91cab04076d746e0718c37970ec16d46cd90a9ce
vegter <amsjurien@gmail.com>
2020-03-06 14:39:59 +0100

    Improve filtering and sorting

---
+++ src/components/FilterItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="text-left">
+      <b-btn block v-b-toggle="title" variant="primary" class="mb-2">
+        {{ title }}
+        <span class="when-closed float-right">
+          <font-awesome-icon icon="chevron-down" />
+        </span>
+        <span class="when-opened float-right">
+          <font-awesome-icon icon="chevron-up" />
+        </span>
+      </b-btn>
+    </div>
+    <b-collapse :id="title" class="text-left">
+      <slot></slot>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterItem",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+.collapsed > .when-opened,
+:not(.collapsed) > .when-closed {
+  display: none;
+}
+</style>
