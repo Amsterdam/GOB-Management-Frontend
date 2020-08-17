45dd029a9af75859f382ecee858201cf29d1ac7e
vegter <amsjurien@gmail.com>
2020-08-17 15:54:58 +0200

    Show GOB status in menu
    Show any number of waiting and processed messages
    Also show any database locks

--- src/components/common/AppHeader.js
+++ src/components/common/AppHeader.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import {useLocation, useHistory, Link} from 'react-router-dom';
+import {useLocation, Link} from 'react-router-dom';
 
 import {Header} from "@datapunt/asc-ui";
 import {MenuButton, MenuInline, MenuItem} from "@datapunt/asc-ui";
 import auth from "../../services/auth";
 import './menu.css'
+import StatusIcons from "../status/StatusIcons";
+
 
 function AppHeader() {
     const location = useLocation();
-    const history = useHistory();
 
     const [user, setUser] = useState(null);
 
@@ -61,10 +62,6 @@ function AppHeader() {
         }
     }
 
-    function go(pathname) {
-        history.push(pathname);
-    }
-
     const navigation = (
         <MenuInline>
             {Object.entries(menuItems).map(([key, value]) => (
@@ -72,13 +69,14 @@ function AppHeader() {
                     <Link to={value} className={'menulink'}>
                         <MenuButton active={isActive(value)}>
                             {key}
+                            {key === 'Status' ? <StatusIcons className="statusicons"></StatusIcons> : ''}
                         </MenuButton>
                     </Link>
                 </MenuItem>
             ))}
             <MenuItem key="gebruiker">
                 <MenuButton onClick={() => user ? logout() : login()}
-                            active={false}>
+                            active={false} className={'mb-1'}>
                     {user ? "Logout" : "Login"}
                 </MenuButton>
             </MenuItem>
